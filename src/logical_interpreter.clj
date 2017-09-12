4b1c88e27258e3205c5be2638283d5eaeb29e9fe
NicolasLercari <niko06_14@hotmail.com>
2017-09-12 14:11:06 -0300

    agrego casos bordes en el parser

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -7,7 +7,12 @@
 
 (defprotocol Operacion
   (comparar [this query bdd])
-   (esta [query bdd])
+
+    (esta [query bdd]
+       "compara cada elemento de la bdd por el query. Retorna true 
+       si la query esta en la base de datos"
+       (boolean (not= (find-first #(comparar % query bdd) bdd) nil))    
+     )
   )
 
 (defrecord Fact [name parametros]
@@ -31,7 +36,7 @@
   (comparar [this query bdd]
     "compara el nombre de la rule con el nombre del query y la cantidad de parametros, si esto es cierto
      procede a especializar cada fact del lado derecho de la rule con los parametros del query pedido
-     y compara con todos estos facts creados.."
+     y verifica que los facts esten en la base de datos."
     ( if (not (and (boolean (= (compare (count (:parametros this)) (count(:parametros query))) 0)) 
          (boolean (= (compare (:name this) (:name query)) 0))))
           false
@@ -42,11 +47,7 @@
             (reduce (fn [x y] ( and x y )) (into [] (map (fn [unFact] (esta unFact bdd)) facts)))
           )    
       )   
-    )
-    (esta [query bdd]
-       "compara cada elemento de la bdd por el query."
-       (boolean (not= (find-first #(comparar % query bdd) bdd) nil))    
-     )      
+    )      
   )
 
 
@@ -56,28 +57,31 @@
       "recibe un string que representa a un fact con el siguiente formato
        add(one, one, two). retorna una lista con nombre de la operacion 
        del fact y los parametros"        
-      (def fact (clojure.string/replace factStr #"," ""))
-      (def fact (clojure.string/replace fact #"\." ""))
+      ;(def fact (clojure.string/replace factStr #"," ""))
+      (def fact (clojure.string/replace factStr #"\." ""))
       (def fact (clojure.string/split (clojure.string/replace fact #"\)" "") #"\(" ))
-      (def nombreFact (get fact 0))
-      (def paramFact (clojure.string/split (get fact 1) #" "))
+      (def nombreFact (clojure.string/trim (get fact 0)))
+      (def paramFact (into [] (map (fn [x] (clojure.string/trim x)) (clojure.string/split (get fact 1) #","))))
+      ;(def paramFact (clojure.string/split (get fact 1) #","))
       [nombreFact paramFact]
   )
 
 (defn crearFact [factStr]
-
+  "crea una instancia de fact."
   (def nombreParamFact (parsear-fact factStr))
   (new Fact (get nombreParamFact 0) (get nombreParamFact 1))
   )
 
-(defn parsear-rule 
-    [ruleStr]
-
+(defn parsear-rule [ruleStr]
+    " recibe un string que representa a la rule con el formato
+      rule(x, y, z) :- factUno(x, y, z), factDos(x, y, z)"
     (def lista (clojure.string/split ruleStr #":-"))
     (def rule (clojure.string/trim (get lista 0)))
     (def rule (crearFact rule))    
     (def facts (clojure.string/trim (get lista 1)))
-    (def facts (into [] (map (fn [x] (clojure.string/trim x)) (clojure.string/split facts #"\)") ) ))
+    ;(def facts (into [] (map (fn [x] (clojure.string/trim x)) (clojure.string/split facts #"\)") ) ))
+    (def facts (into [] (map (fn [x] (clojure.string/trim x)) (clojure.string/split facts #"\)(\ )*,") )))
+
     (def facts (into [] (map (fn [x] (crearFact x ) ) facts)))
     [(:name rule) (:parametros rule) facts]
   )
@@ -93,7 +97,7 @@
     (def data (clojure.string/trim database))
     (def data2 (clojure.string/replace data #"\n" ""))
     (def data2 (clojure.string/replace data2 #"\t" ""))
-    (def data2 (clojure.string/replace data2 #"," ""))
+    ;(def data2 (clojure.string/replace data2 #"," ""))
     ;(def data2 (clojure.string/replace data2 #"\." ""))
     (clojure.string/split data2 #"\." )
 )
@@ -117,5 +121,7 @@
     (def bdd (crearBaseDeDatos database))
     (def queryFact (crearFact query))
     (esta queryFact bdd)
+    ;bdd
+
    (catch Exception e nil))
 )
